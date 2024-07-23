5a9c67da67184be854633f9d3123476cc1053508
Minahil-Areej <minahil.areej24@gmail.com>
2024-07-23 21:12:04 +0100

    Delete book

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -27,7 +27,7 @@ const CreateBooks = () => {
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Chack console');
+        alert('An error happened. Please Check console');
         console.log(error);
       });
   };
@@ -73,4 +73,4 @@ const CreateBooks = () => {
   );
 }
 
-export default CreateBooks
+export default CreateBooks;
