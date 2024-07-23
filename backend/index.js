86066cf91466a20865c333572e981a43a68a1395
Minahil-Areej <minahil.areej24@gmail.com>
2024-07-23 15:54:27 +0100

    Added react frontend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,23 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Book} from './models/bookModel.js';
 import booksRoutes from './routes/booksRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 //middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling cors
+//Option:1 - Allow all origins with default of cors(*)
+//app.use(cors());
+//Option:2 - Allow custom origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+}))
 
 app.get('/', (request, response) => {
     console.log(request);
